0ac3c96680e32b7d0d6ed8ca29775c8f06c9b227
auilk <abdelghafour.lbaz01@gmail.com>
2025-06-12 12:14:08 +0100

    Add custom fonts and configure them for project use

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App()
 {
   return(
     <div className="w-dvw h-dvh flex justify-center items-center">
-      <h1 className="text-9xl font-bold">NEUNET</h1>
+      <h1 className="text-9xl font-jetbrains-mono">NEUNET</h1>
     </div>
   );
 }
