58fae59eb5a0517d3efa41d5623cde1a2a3eaabb
auilk <abdelghafour.lbaz01@gmail.com>
2025-06-22 21:22:33 +0100

    Improve useLerp hook and rename it to useTween for clearer naming

---
+++ src/hook/useTween.js
@@ -0,0 +1,66 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const lerps = new Map();
+let animReqRef = null;
+
+/**
+ * A custom React hook that tween-animates a numeric value over time using linear interpolation.
+ * 
+ * @function useTween
+ * @param {number} initialValue - The starting value for the tween.
+ * @returns {[number, (to: number, duration: number) => void]} A tuple:
+ *  - "value": The current tweened value.
+ *  - "tweenTo": A function to animate to a new target value over the specified duration (in milliseconds).
+ */
+function useTween(initialValue)
+{
+    const [value, setValue] = useState(initialValue);
+    const idRef = useRef(Symbol());
+
+    const tweenTo = useCallback((to, duration) =>
+    {
+        const start = performance.now();
+        lerps.set(idRef.current, { from: value, to, duration, start, setValue });
+
+        if (!animReqRef)
+        {
+            const Loop = (now) =>
+            {
+                const finished = [];
+
+                lerps.forEach((lerp, key) =>
+                {
+                    const elapsed = now - start;
+                    const time = Math.min(elapsed / lerp.duration, 1);
+                    setValue(lerp.from + (lerp.to - lerp.from) * time);
+
+                    if (time >= 1) finished.push(key);
+                });
+
+                finished.forEach((key) => lerps.delete(key));
+
+                if (lerps.size > 0) animReqRef = requestAnimationFrame(Loop);
+                else animReqRef = null;
+            }
+
+            animReqRef = requestAnimationFrame(Loop);
+        }
+    }, [value]);
+
+    useEffect(() =>
+    {
+        return () =>
+        {
+            lerps.delete(idRef.current);
+            if (lerps.size === 0 && animReqRef)
+            {
+                cancelAnimationFrame(animReqRef);
+                animReqRef = null;
+            }
+        };
+    }, []);
+
+    return [value, tweenTo];
+}
+
+export default useTween;
